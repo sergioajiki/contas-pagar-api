e87135172afc7d1d5505ac2b98e699109b262a3f
Sergio Ajiki <sergioajk@hotmail.com>
2024-07-02 11:22:10 -0300

    initial commit contas-pagar-api

---
+++ src/main/java/projetos/contas_pagar_api/ContasPagarApiApplication.java
@@ -0,0 +1,13 @@
+package projetos.contas_pagar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasPagarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasPagarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetos/contas_pagar_api/ContasPagarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.contas_pagar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasPagarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
