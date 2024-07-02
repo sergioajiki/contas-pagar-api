441599665211cb6aaac8d41e4f8af37d36ddf3b7
Sergio Ajiki <sergioajk@hotmail.com>
2024-07-02 19:25:07 -0300

    feat: LancamentoService - utilizando variavel var nos metodos em substituião a Optionl<>

--- src/main/java/projetos/contas_pagar_api/service/FornecedorService.java
+++ src/main/java/projetos/contas_pagar_api/service/FornecedorService.java
@@ -1,6 +1,8 @@
 package projetos.contas_pagar_api.service;
 
 import org.springframework.stereotype.Service;
+import projetos.contas_pagar_api.advice.exception.DuplicateEntryException;
+import projetos.contas_pagar_api.advice.exception.NotFoundException;
 import projetos.contas_pagar_api.model.entity.Fornecedor;
 import projetos.contas_pagar_api.model.repository.FornecedorRepository;
 
@@ -24,22 +26,22 @@ public class FornecedorService implements IFornecedorService {
     public Fornecedor findById(Long id) {
         Optional<Fornecedor> fornecedorOptional = fornecedorRepository.findById(id);
         if (fornecedorOptional.isEmpty()) {
-            throw new RuntimeException("Fornecedor não encontrado");
+            throw new NotFoundException(String.format("Fornecedor com id %d não encontrado", id));
         }
         return fornecedorOptional.get();
     }
     public Fornecedor create(Fornecedor fornecedorToCreate) {
         Optional<Fornecedor> verificaCpfOptional = Optional.ofNullable(fornecedorRepository.findByCpf(fornecedorToCreate.getCpf()));
         if (verificaCpfOptional.isPresent()) {
-            throw new RuntimeException("Cpf já está cadastrado");
+            throw new DuplicateEntryException("Cpf já está cadastrado");
         }
         Optional<Fornecedor> verificaCnpjOptional = Optional.ofNullable(fornecedorRepository.findByCnpj(fornecedorToCreate.getCnpj()));
         if (verificaCnpjOptional.isPresent()) {
-            throw new RuntimeException("Cnpj já está cadastrado");
+            throw new DuplicateEntryException("Cnpj já está cadastrado");
         }
         Optional<Fornecedor> verificaEmailOptional = Optional.ofNullable(fornecedorRepository.findByEmail(fornecedorToCreate.getEmail()));
         if (verificaEmailOptional.isPresent()) {
-            throw new RuntimeException("Email já está cadastrado");
+            throw new DuplicateEntryException("Email já está cadastrado");
         }
 
         fornecedorRepository.save(fornecedorToCreate);
@@ -50,7 +52,7 @@ public class FornecedorService implements IFornecedorService {
     public Fornecedor update(Long id, Fornecedor fornecedorToUpdate) {
         Optional<Fornecedor> fornecedorOptional = fornecedorRepository.findById(id);
         if(fornecedorOptional.isEmpty()) {
-            throw new RuntimeException("Fornecedor com id não encontrado");
+            throw new NotFoundException(String.format("Fornecedor com id %d não encontrado", id));
         }
 
         fornecedorRepository.save(fornecedorToUpdate);
@@ -61,7 +63,7 @@ public class FornecedorService implements IFornecedorService {
     public void delete(Long id) {
         Optional<Fornecedor> fornecedorOptional = fornecedorRepository.findById(id);
         if(fornecedorOptional.isEmpty()) {
-            throw new RuntimeException("Fornecedor com id não encontrado");
+            throw new NotFoundException(String.format("Fornecedor com id %d não encontrado", id));
         }
         fornecedorRepository.delete(fornecedorOptional.get());
     }
---
+++ src/main/java/projetos/contas_pagar_api/service/LancamentoService.java
@@ -0,0 +1,59 @@
+package projetos.contas_pagar_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import projetos.contas_pagar_api.advice.exception.NotFoundException;
+import projetos.contas_pagar_api.model.entity.Fornecedor;
+import projetos.contas_pagar_api.model.entity.Lancamento;
+import projetos.contas_pagar_api.model.repository.LancamentoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LancamentoService implements ILancamentoService {
+    private LancamentoRepository lancamentoRepository;
+
+    @Autowired
+    public LancamentoService(LancamentoRepository lancamentoRepository) {
+        this.lancamentoRepository = lancamentoRepository;
+    }
+
+    @Override
+    public List findAll() {
+        return this.lancamentoRepository.findAll();
+    }
+
+    @Override
+    public Lancamento findById(Long id) {
+        Optional<Lancamento> lancamentoOptional = lancamentoRepository.findById(id);
+        if (lancamentoOptional.isEmpty()) {
+            throw new NotFoundException(String.format("Lançamento com id %d não encontrado", id));
+        }
+        return lancamentoOptional.get();
+    }
+
+    @Override
+    public Lancamento create(Lancamento lancamentoToCreate) {
+        lancamentoRepository.save(lancamentoToCreate);
+        return lancamentoToCreate;
+    }
+
+    @Override
+    public Lancamento update(Long id, Lancamento lancamentoToUpdate) {
+        var lancamentoOptional = lancamentoRepository.findById(id);
+        if (lancamentoOptional.isEmpty()) {
+            throw new NotFoundException(String.format("Lançamento com id %d não encontrado", id));
+        }
+        lancamentoRepository.save(lancamentoToUpdate);
+
+        return lancamentoToUpdate;
+    }
+
+    @Override
+    public void delete(Long id) {
+        var lancamentoOptional = lancamentoRepository.findById(id);
+        if (lancamentoOptional.isEmpty()) {
+            throw new NotFoundException(String.format("Lançamento com id %d não encontrado", id));
+        }
+        lancamentoRepository.delete(lancamentoOptional.get());
+    }
+}
